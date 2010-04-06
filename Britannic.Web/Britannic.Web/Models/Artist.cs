563f6105122508ca4769febf09b38aaacbe717bb
BenHall <ben@benhall.me.uk>
2010-04-07 00:06:54 +0100

    Changed mapping for Photos -> Photo in CouchDb document

--- Britannic.Web/Britannic.Web/Models/Artist.cs
+++ Britannic.Web/Britannic.Web/Models/Artist.cs
@@ -7,6 +7,7 @@ namespace Britannic.Web.Models
     public class Artist
     {
         public string Name { get; set; }
+        [JsonProperty("photo")]
         public IList<string> Photos { get; set; }
         public IList<string> Tags { get; set; }
         public string Website { get; set; }
